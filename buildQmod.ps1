642df8b20b22f6ee46ed00b60a7177c0daaf6452
FernTheDev <15272073+Fernthedev@users.noreply.github.com>
2021-07-09 21:20:59 -0400

    Version bump

--- buildQmod.ps1
+++ buildQmod.ps1
@@ -2,7 +2,7 @@
 cargo ndk build --release
 
 if ($?) {
-    Compress-Archive -Path "./mod.json", "./target/aarch64-linux-android/release/libpinkcute.so" -DestinationPath "./pink_cute_v0.1.0.zip" -Update
-    Remove-Item "./pink_cute_v0.1.0.qmod"
-    Rename-Item "./pink_cute_v0.1.0.zip" "./pink_cute_v0.1.0.qmod"
+    Compress-Archive -Path "./mod.json", "./target/aarch64-linux-android/release/libpinkcute.so" -DestinationPath "./pink_cute_v0.1.1.zip" -Update
+    Remove-Item "./pink_cute_v0.1.1.qmod"
+    Rename-Item "./pink_cute_v0.1.1.zip" "./pink_cute_v0.1.1.qmod"
 }
